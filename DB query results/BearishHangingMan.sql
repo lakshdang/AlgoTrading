a10e8585d43af8f8155c50b3a0c647b220a2b595
Laksh <laksh.dang@gmail.com>
2018-02-19 13:40:25 -0500

    Bearish Hanging Man updates

--- DB query results/BearishHangingMan.sql
+++ DB query results/BearishHangingMan.sql
@@ -41,27 +41,39 @@
 -- AND
 -- candles.timestamp < date_add(HangingManCandles.candleTime, interval 301 minute));
 
-Select t1.companyID, t1.candleTime, t1.closePrice, t1.timestamp as MaxCandleTime, t1.High from 
-	temp as t1 
-	join 
-	(
-		Select candleID, max(Close) as maxClose -- max(High) as maxHigh-- , min(Open) as minOpen, min(Close) as minClose 
-        from
-        temp
-        Group By candleID
-        
-    )t2
-	on
-	t1.candleID = t2.candleID
-	and
-	t1.Close = t2.maxClose
-    Order by t1.companyID, t1.candleTime , t1.timestamp;
 
-Select count(*) as count from
-(
-	Select candleID from temp
-    Group By candleID
-)t;
+
+
+-- Select t.*, Company_Map.Name from
+-- (Select t1.companyID, t1.candleTime as MainCandleTime, t1.closePrice as MainCandleClose, t1.timestamp as MinCandleTime, t1.Close as MinCLose, (t1.closePrice-t2.minClose) as MaxLoss from 
+-- 	temp as t1 
+-- 	join 
+-- 	(
+-- 		Select candleID, min(Close) as minClose -- max(Close) as maxClose -- max(High) as maxHigh-- , min(Open) as minOpen, min(Close) as minClose 
+--         from
+--         temp
+--         Group By candleID
+--         
+--     )t2
+-- 	on
+-- 	t1.candleID = t2.candleID
+-- 	and
+-- 	t1.Close = t2.minClose
+--     Order by t1.companyID, t1.candleTime , t1.timestamp)t
+--     join Company_Map
+--     on
+--     t.companyID = Company_Map.id_Company_Map;
+
+
+
+
+
+-- 
+-- Select count(*) as count from
+-- (
+-- 	Select candleID from temp
+--     Group By candleID
+-- )t;
 
 -- Select * from
 -- (
@@ -96,3 +108,11 @@ Select count(*) as count from
 -- 	group by  candleID, companyID, candleTime, closePrice
 -- )HangingManCandles
 -- where time(HangingManCandles.candleTime) =  time("2015-08-25 15:15:00")
+
+
+-- Select * from 15_min_candles where company_id = 2 Order By timestamp 
+-- Select t2.* from 
+-- (Select min(id_15_min_candles) as id_15_min_candles  from 15_min_candles)t1
+-- Join
+-- 15_min_candles t2
+-- on t2.id_15_min_candles = t1.id_15_min_candles
--- test.js
+++ test.js
@@ -1,23 +1,77 @@
-module.exports = {
-	A: function(callback){
-		console.log('A');
-		setTimeout(function () {
-  		callback();
-		}, 10000);
-	},
+var mysql = require('mysql');
+var dbconfig = require('./config/dbconfig');
+var connection = mysql.createConnection(dbconfig.sql_db_info);
+var dir = "./15MinCandleData/";
+var fs = require('fs');
+var path = require('path');
 
-	B: function(){
-		console.log('B');
-	},
+// var data = require("./15MinCandleData/ACC.json");
 
-	temp: function(connection, count){
-		connection.query('Select * from 15_min_candles Where company_id = 1', function(error, results, fields){
-			if(error)throw error;
-			console.log(count + " " + results.length);
-			if(count<3)
-				temp(connection, count+1)
-			else
-				console.log("Done");
-		});
-	}
+// var 
+// 	data.candles.forEach(function(candle){
+// 		var curr = [];
+// 		curr.push(1);
+// 		curr.push(timeStampFormat(candle[0]));
+// 		curr.push(candle[1]);
+// 		curr.push(candle[4]);
+// 		curr.push(candle[2]);
+// 		curr.push(candle[3]);
+// 		curr.push(candle[5]);
+// 		values.push(curr);
+// 	});
+
+fs.readdir(dir, function(err, files){
+	var companyId = 0;
+	if(err)
+		throw err;
+	files.forEach(function(file, index){
+		if(file.indexOf('.json')>-1){
+			var values = [];
+			companyId++;
+			var data = require(dir + file);
+			var candles = data.candles;
+			// console.log(companyId + " " + data.candles.length);
+			
+			candles.forEach(function(candle){
+				var curr = [];
+				curr.push(companyId);
+				curr.push(timeStampFormat(candle[0]));
+				curr.push(candle[1]);
+				curr.push(candle[4]);
+				curr.push(candle[2]);
+				curr.push(candle[3]);
+				curr.push(candle[5]);
+				values.push(curr);
+			});
+
+			connection.connect();
+			connection.query('Insert into 15_min_candles_test (company_id, timestamp, Open, Close, High, Low, Volume) Values ?', [values], function(error, result, fields){
+				if(error)throw error;
+				else console.log(result);
+			});
+			connection.end();
+		}
+	})
+	// console.log("Total candles: " + values.length);
+
+
+});
+
+
+// connection.connect();
+
+// connection.query('Insert into 15_min_candle_test (company_id, timestamp, Open, Close, High, Low, Volume) Values ?', [values], function(error, result, fields){
+// 	if(error)throw error;
+// 	else console.log(result);
+// });
+
+// connection.end();
+
+
+// helper functions
+
+function timeStampFormat(orig){
+	var ret = orig.substring(0, 10) + " " + orig.substring(11, 19);
+	// console.log(ret);
+	return ret;
 }
