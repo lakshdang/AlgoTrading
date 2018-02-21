2340d6bed17f78b69bb8fad399de80f964e6ba68
Laksh <laksh.dang@gmail.com>
2018-02-21 00:15:27 -0500

    Bearish Hanging Man updates 2018-2-21

--- server.js
+++ server.js
@@ -8,7 +8,12 @@ var dir = "./15MinCandleData";
 // var data = require("./15MinCandleData/ICICIPRULI.json");
 // console.log(data.candles.length);
 
-// connection.connect();
+connection.connect();
 // DB_data_entry.insertCompanies(connection, dir);
-Hangingman.BearishHanginMan(connection);
-// connection.end();
+// Hangingman.BearishHanginMan(connection);
+
+connection.query("Select * from 15_min_candles t1 Order by t1.company_id, t1.timestamp", function(error, results, fields){
+	if(error)throw error;
+	console.log(results);
+})
+connection.end();
